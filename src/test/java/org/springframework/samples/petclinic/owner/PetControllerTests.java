fdf3ade21160880fae9321c5952d9f416b2bf5ba
hamidkhd <hrkh78@gmail.com>
2021-12-09 20:29:58 +0330

    Add PetControllerTests.

--- src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
@@ -1,5 +1,178 @@
 package org.springframework.samples.petclinic.owner;
 
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.samples.petclinic.utility.PetTimedCache;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(value = PetController.class,
+	includeFilters = {
+		@ComponentScan.Filter(value = PetTypeFormatter.class, type = FilterType.ASSIGNABLE_TYPE),
+		@ComponentScan.Filter(value = PetService.class, type = FilterType.ASSIGNABLE_TYPE),
+		@ComponentScan.Filter(value = LoggerConfig.class, type = FilterType.ASSIGNABLE_TYPE),
+		@ComponentScan.Filter(value = PetTimedCache.class, type = FilterType.ASSIGNABLE_TYPE),
+	}
+)
+
 class PetControllerTests {
 	// TODO
+	private static final int OWNER_ID = 1;
+	private static final int PET_ID = 1;
+	private static final int PET_TYPE_ID = 5;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private PetRepository pets;
+
+	@MockBean
+	private OwnerRepository owners;
+
+	@BeforeEach
+	void setup() {
+		PetType cat = new PetType();
+		cat.setId(PET_TYPE_ID);
+		cat.setName("CAT");
+
+		Pet pet = new Pet();
+		pet.setId(PET_ID);
+
+		Owner owner = new Owner();
+		owner.setId(OWNER_ID);
+
+		given(pets.findById(PET_ID)).willReturn(pet);
+		given(pets.findPetTypes()).willReturn(Lists.newArrayList(cat));
+		given(owners.findById(OWNER_ID)).willReturn(owner);
+	}
+
+
+	@Test
+	void testInitCreationFormSuccess() throws Exception {
+		mockMvc.perform(get("/owners/{ownerId}/pets/new", OWNER_ID)
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("pet"))
+			.andExpect(view().name("pets/createOrUpdatePetForm"));
+	}
+
+	@Test
+	void testProcessCreationFormSuccess() throws Exception {
+		mockMvc.perform(post("/owners/{ownerId}/pets/new", OWNER_ID)
+				.param("name", "Jhon")
+				.param("type", "CAT")
+				.param("birthDate", "2000-01-01"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/owners/{ownerId}"));
+	}
+
+	@Test
+	void testProcessCreationFormFailInType() throws Exception {
+		mockMvc.perform(post("/owners/{ownerId}/pets/new", OWNER_ID)
+				.param("name", "Jhon")
+				.param("birthDate", "2000-01-01"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeHasNoErrors("owner"))
+			.andExpect(model().attributeHasErrors("pet"))
+			.andExpect(model().attributeHasFieldErrors("pet", "type"))
+			.andExpect(model().attributeHasFieldErrorCode("pet", "type", "required"))
+			.andExpect(view().name("pets/createOrUpdatePetForm"));
+	}
+
+	@Test
+	void testProcessCreationFormFailInName() throws Exception {
+		mockMvc.perform(post("/owners/{ownerId}/pets/new", OWNER_ID)
+				.param("type", "CAT")
+				.param("birthDate", "2000-01-01"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeHasNoErrors("owner"))
+			.andExpect(model().attributeHasErrors("pet"))
+			.andExpect(model().attributeHasFieldErrors("pet", "name"))
+			.andExpect(model().attributeHasFieldErrorCode("pet", "name", "required"))
+			.andExpect(view().name("pets/createOrUpdatePetForm"));
+	}
+
+	@Test
+	void testProcessCreationFormFailInBirthDate() throws Exception {
+		mockMvc.perform(post("/owners/{ownerId}/pets/new", OWNER_ID)
+				.param("name", "Jhon")
+				.param("type", "CAT"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeHasNoErrors("owner"))
+			.andExpect(model().attributeHasErrors("pet"))
+			.andExpect(model().attributeHasFieldErrors("pet", "birthDate"))
+			.andExpect(model().attributeHasFieldErrorCode("pet", "birthDate", "required"))
+			.andExpect(view().name("pets/createOrUpdatePetForm"));
+	}
+
+	@Test
+	void testInitUpdateFormSuccess() throws Exception {
+		mockMvc.perform(get("/owners/{ownerId}/pets/{petId}/edit", OWNER_ID, PET_ID)
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("pet"))
+			.andExpect(view().name("pets/createOrUpdatePetForm"));
+	}
+
+	@Test
+	void testProcessUpdateFormSuccess() throws Exception {
+		mockMvc.perform(post("/owners/{ownerId}/pets/{petId}/edit", OWNER_ID, PET_ID)
+				.param("name", "Jhon")
+				.param("type", "CAT")
+				.param("birthDate", "2000-01-01"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/owners/{ownerId}"));
+	}
+
+	@Test
+	void testProcessUpdateFormFailInType() throws Exception {
+		mockMvc.perform(post("/owners/{ownerId}/pets/{petId}/edit", OWNER_ID, PET_ID)
+				.param("name", "Betty")
+				.param("birthDate", "2000-01-01"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeHasNoErrors("owner"))
+			.andExpect(model().attributeHasErrors("pet"))
+			.andExpect(model().attributeHasFieldErrors("pet", "type"))
+			.andExpect(model().attributeHasFieldErrorCode("pet", "type", "required"))
+			.andExpect(view().name("pets/createOrUpdatePetForm"));
+	}
+
+	@Test
+	void testProcessUpdateFormFailInName() throws Exception {
+		mockMvc.perform(post("/owners/{ownerId}/pets/{petId}/edit", OWNER_ID, PET_ID)
+				.param("type", "CAT")
+				.param("birthDate", "2000-01-01"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeHasNoErrors("owner"))
+			.andExpect(model().attributeHasErrors("pet"))
+			.andExpect(model().attributeHasFieldErrors("pet", "name"))
+			.andExpect(model().attributeHasFieldErrorCode("pet", "name", "required"))
+			.andExpect(view().name("pets/createOrUpdatePetForm"));
+	}
+
+	@Test
+	void testProcessUpdateFormFailInBirthDate() throws Exception {
+		mockMvc.perform(post("/owners/{ownerId}/pets/{petId}/edit", OWNER_ID, PET_ID)
+				.param("name", "Betty")
+				.param("type", "CAT"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeHasNoErrors("owner"))
+			.andExpect(model().attributeHasErrors("pet"))
+			.andExpect(model().attributeHasFieldErrors("pet", "birthDate"))
+			.andExpect(model().attributeHasFieldErrorCode("pet", "birthDate", "required"))
+			.andExpect(view().name("pets/createOrUpdatePetForm"));
+	}
 }
